539db2184bfa3fd040d84c9bc5bcfb95bb0bc393
thejf <jf.arseneau@gmail.com>
2021-02-01 09:31:21 -0800

    Add beginning of image reference renaming

--- webify_models/src/mesh_update/process.rs
+++ webify_models/src/mesh_update/process.rs
@@ -7,8 +7,10 @@ pub fn process(dir: &Path) -> std::result::Result<(), std::io::Error> {
   let meshes = mesh_update::scan_dir_for_meshes(dir).unwrap();
   let mesh_bar = cli::create_progress_bar(meshes.len() as u64);
 
+  mesh_bar.set_prefix("Mesh Update");
   for mesh in meshes {
     mesh_bar.inc(1);
+    mesh_bar.set_message(&format!("Updating {:?}...", &mesh));
     mesh_update::rename_image_references(&mesh)?;
   }
   mesh_bar.finish_with_message("Meshes webified!");
--- webify_models/src/mesh_update/rename_image_references.rs
+++ webify_models/src/mesh_update/rename_image_references.rs
@@ -1,7 +1,31 @@
+use std::fs;
 use std::path::PathBuf;
 
+use aho_corasick::AhoCorasickBuilder;
+
 pub fn rename_image_references(mesh: &PathBuf) -> std::result::Result<(), std::io::Error> {
-  println!("{:?}", mesh);
+  let result = find_and_rename_image_references(mesh)?;
+  let final_result = update_texture_path(result)?;
+  fs::write(mesh, final_result)?;
 
   Ok(())
+}
+
+fn find_and_rename_image_references(mesh: &PathBuf) -> std::result::Result<String, std::io::Error> {
+  let patterns = &[".jpg", "_tga"];
+  let f = fs::read_to_string(mesh)?;
+
+  let ac = AhoCorasickBuilder::new().build(patterns);
+  let result = ac.replace_all(&f, &[".png", "_png"]);
+
+  Ok(result)
+}
+
+fn update_texture_path(contents: String) -> std::result::Result<String, std::io::Error> {
+  // Find all image references (with PNG)
+  // Get right relative directory path
+  // Prefix image reference with relative directory path to textures
+  // Call it a day (and return the string to the caller)
+
+  Ok(contents)
 }
