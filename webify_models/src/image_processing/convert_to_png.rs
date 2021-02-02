c9873e049120071372ac331e46008009dc2f40b8
thejf <jf.arseneau@gmail.com>
2021-02-01 19:36:24 -0800

    Update image paths in DAE

--- webify_models/src/image_processing/convert_to_png.rs
+++ webify_models/src/image_processing/convert_to_png.rs
@@ -6,9 +6,12 @@ use image::ImageFormat::Tiff;
 use console::style;
 use indicatif::ProgressBar;
 
-use super::image::Image;
+use crate::image_processing::Image;
 
-pub fn convert_to_png(mut image: Image, progress_bar: &ProgressBar) -> std::io::Result<Image> {
+pub fn convert_to_png(
+  image: Image,
+  progress_bar: &ProgressBar,
+) -> std::result::Result<Image, std::io::Error> {
   progress_bar.set_prefix("PNG Conversion");
   let styled_path = style(image.path.to_string_lossy()).dim();
   if image.extension == "png" {
@@ -17,6 +20,13 @@ pub fn convert_to_png(mut image: Image, progress_bar: &ProgressBar) -> std::io::
   }
 
   progress_bar.set_message(&format!("Converting {}...", styled_path));
+  let converted_image = convert(image.clone())?;
+  progress_bar.set_message(&format!("{} converted!", styled_path));
+
+  Ok(converted_image)
+}
+
+fn convert(mut image: Image) -> std::result::Result<Image, std::io::Error> {
   let image_reader = match ImageReader::open(image.path.clone()) {
     Ok(img) => img,
     Err(e) => panic!("Failed to open image during PNG conversion: {:?}", e),
@@ -35,13 +45,7 @@ pub fn convert_to_png(mut image: Image, progress_bar: &ProgressBar) -> std::io::
     };
 
     fs::remove_file(&image.path)?;
-    progress_bar.set_message(&format!("{} converted!", styled_path));
     image.path = image.path.with_extension("png");
-  } else {
-    progress_bar.set_message(&format!(
-      "{} skipped, as conversion of this file is problematic at this moment.",
-      styled_path
-    ));
   }
 
   Ok(image)
--- webify_models/src/mesh_update/rename_image_references.rs
+++ webify_models/src/mesh_update/rename_image_references.rs
@@ -1,11 +1,11 @@
-use std::fs;
+use std::{fs::{self, File}, io::BufReader, path::Path};
 use std::path::PathBuf;
 
 use aho_corasick::AhoCorasickBuilder;
 
 pub fn rename_image_references(mesh: &PathBuf) -> std::result::Result<(), std::io::Error> {
   let result = find_and_rename_image_references(mesh)?;
-  let final_result = update_texture_path(result)?;
+  let final_result = update_texture_path(result, &mesh)?;
   fs::write(mesh, final_result)?;
 
   Ok(())
@@ -21,11 +21,39 @@ fn find_and_rename_image_references(mesh: &PathBuf) -> std::result::Result<Strin
   Ok(result)
 }
 
-fn update_texture_path(contents: String) -> std::result::Result<String, std::io::Error> {
+fn update_texture_path(contents: String, mesh: &PathBuf) -> std::result::Result<String, std::io::Error> {
+  let patterns = &["<init_from>", "</init_from>"];
+  let ac = AhoCorasickBuilder::new().build(patterns);
+  let texture_path = Path::new("materials").join("textures");
+
+  let mut new_contents = String::new();
+  for line in contents.lines() {
+    let mut start = 0;
+    let mut new_line: String = String::new();
+    for mat in ac.find_iter(&line) {
+      if mat.pattern() == 0 { start = mat.end(); }
+      if mat.pattern() == 1 {
+        let texture_name = &line[start..mat.start()];
+        if texture_name.ends_with(".png") && !texture_name.contains(&texture_path.to_str().unwrap()) {
+          // TODO: Properly find the root path of the mesh, rather than assuming
+          new_line = line.replace(texture_name, Path::new("..").join("materials").join("textures").join(texture_name).to_str().unwrap());
+        }
+      }
+    }
+    // TODO: Fix the output of this to keep the newlines
+    if new_line.is_empty() {
+      new_contents.push_str(format!("{}\n", line).as_str());
+    } else {
+      new_contents.push_str(format!("{}\n", new_line).as_str());
+    }
+
+  };
+
+
   // Find all image references (with PNG)
   // Get right relative directory path
   // Prefix image reference with relative directory path to textures
   // Call it a day (and return the string to the caller)
 
-  Ok(contents)
+  Ok(new_contents)
 }
