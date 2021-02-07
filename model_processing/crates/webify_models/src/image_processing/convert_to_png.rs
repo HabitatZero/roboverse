f008e9dc908e397698bdd1a0c0d55190b5c23691
thejf <jf.arseneau@gmail.com>
2021-02-07 15:22:04 -0800

    Add a few tests to convert_to_png

--- model_processing/crates/webify_models/src/image_processing/convert_to_png.rs
+++ model_processing/crates/webify_models/src/image_processing/convert_to_png.rs
@@ -1,6 +1,10 @@
 //! Converts an image file to PNG, or skips if it's already PNG
 
-use std::fs;
+use std::{
+  fs,
+  path::Path,
+  panic,
+};
 
 use image::io::Reader as ImageReader;
 use image::ImageFormat::Tiff;
@@ -50,7 +54,77 @@ fn convert(mut image: Image) -> std::result::Result<Image, std::io::Error> {
 
     fs::remove_file(&image.path)?;
     image.path = image.path.with_extension("png");
+  } else {
+    panic!("Failed to convert provided image: {:?}", image.path);
   }
 
   Ok(image)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  fn setup(test_run_id: &str) -> std::result::Result<(), std::io::Error> {
+    let example_image_path = Path::new("tests").join("image_processing").join("images");
+    let destination_path = Path::new("tests").join("image_processing").join(test_run_id);
+    fs::create_dir_all(&destination_path)?;
+    fs::copy(example_image_path.join("example.jpg"), &destination_path.join("example.jpg"))?;
+    fs::copy(example_image_path.join("README.md"), &destination_path.join("README.md"))?;
+
+    Ok(())
+  }
+
+  fn teardown(test_run_id: &str) -> std::result::Result<(), std::io::Error> {
+    let destination_path = Path::new("tests").join("image_processing").join(test_run_id);
+    fs::remove_dir_all(destination_path)?;
+
+    Ok(())
+  }
+
+  #[test]
+  fn it_converts_a_jpg_to_png() -> std::result::Result<(), std::io::Error> {
+    let test_run_name = "test_run_it_converts_a_jpg_to_png";
+    setup(test_run_name)?;
+
+    let test_image_path = Path::new("tests").join("image_processing").join(test_run_name).join("example.jpg");
+    assert!(Path::exists(&test_image_path));
+
+    let image = Image {
+      path: test_image_path,
+      extension: String::from("jpg"),
+    };
+
+    convert(image)?;
+    // Check that previous test image was deleted
+    assert!(!Path::exists(&Path::new("tests").join("image_processing").join(test_run_name).join("example.jpg")));
+    // Check that new image is there as a PNG
+    assert!(Path::exists(&Path::new("tests").join("image_processing").join(test_run_name).join("example.png")));
+
+    teardown(test_run_name)?;
+    Ok(())
+  }
+
+  #[test]
+  fn it_panics_on_non_images() {
+    let test_run_name = "test_run_it_panics_on_non_images";
+    setup(test_run_name);
+
+    let test_image_path = Path::new("tests").join("image_processing").join(test_run_name).join("README.md");
+    assert!(Path::exists(&test_image_path));
+
+    let non_image = Image {
+      path: test_image_path,
+      extension: String::from("jpg"),
+    };
+
+    // Catch the panic here so we can teardown after, otherwise
+    // just using should_panic will leave use with no teardown
+    let result = panic::catch_unwind(|| {
+      convert(non_image);
+    });
+    assert!(result.is_err());
+
+    teardown(test_run_name);
+  }
 }
